ac9a04de9aae79b0c313e41331be7cc4f0202240
poojamanjeshwar <poojamanjeshwar1992@gmail.com>
2017-08-23 21:40:32 -0400

    Contact List Files

---
+++ public/controllers/controller.js
@@ -0,0 +1,50 @@
+angular.module("Contacts", []).controller("AppCtrl", AppCtrlFunc)
+
+function AppCtrlFunc($scope, $http) {
+    console.log("hello world");
+
+    var refresh = function () {
+
+        $http.get('/contactlist').then(function (response) {
+            console.log("I got the data i requested");
+            $scope.contactlist = response.data;
+            $scope.contact = null;
+        });
+    };
+
+    refresh();
+
+    $scope.addContact = function () {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).then(function (response) {
+            console.log(response.data);
+            refresh();
+        });
+    };
+
+    $scope.remove = function (id) {
+        console.log(id);
+        $http.delete('/contactlist/' + id).then(function (response) {
+            refresh();
+        });
+
+    };
+
+    $scope.edit = function(id){
+        console.log(id);
+        $http.get('/contactlist/' + id).then(function (response) {
+            $scope.contact = response.data;
+        });
+    };
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response){
+            refresh();
+        });
+    };
+
+    $scope.deselect = function(){
+        $scope.contact = null;
+    }
+}
